3ac09931a96806192a2980ab5061e4c4a90dfc75
mertgunel98 <mertgunel55@gmail.com>
2026-06-04 02:53:03 +0300

    chore: add bash start script for cleaner startup sequence

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+echo "==> Running collectstatic..."
+python manage.py collectstatic --noinput
+
+echo "==> Running migrations..."
+python manage.py migrate --noinput
+
+echo "==> Seeding games..."
+python manage.py seed_games
+
+echo "==> Starting Gunicorn..."
+exec gunicorn gameportal.wsgi --bind 0.0.0.0:$PORT --workers 2 --log-file -
